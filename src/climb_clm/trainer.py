176b224d7c1b74f860dd88862ecb4d8146427399
Rob Parrett <robparrett@gmail.com>
2024-08-02 05:21:09 -0700

    WIP: custom loss func

---
+++ src/climb_clm/trainer.py
@@ -0,0 +1,93 @@
+from torch import nn, argmax
+from transformers import Trainer
+import pprint
+
+class CustomTrainer(Trainer):
+    start_hold_tokens = set()
+    end_hold_tokens = set()
+    any_hold_tokens = set()
+    angle_tokens = set()
+    difficulty_tokens = set()
+
+    def __init__(self, *args, tokenizer, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for s, t in tokenizer.get_vocab().items():
+            if "r12" in s:
+                self.start_hold_tokens.add(t)
+            elif "r14" in s:
+                self.end_hold_tokens.add(t)
+            elif "r13" in s:
+                self.any_hold_tokens.add(t)
+            elif "a" in s:
+                self.angle_tokens.add(t)
+            elif "d" in s:
+                self.difficulty_tokens.add(t)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        (loss, outputs) = super().compute_loss(model, inputs, return_outputs=True)
+
+        logits = outputs.logits
+
+        # Compute custom penalties
+        predictions = argmax(logits, dim=-1)
+        penalties = self.compute_penalties(predictions)
+
+        loss += penalties
+
+        return (loss, outputs) if return_outputs else loss
+
+    def compute_penalties(self, predictions):
+        penalty_factor = 0.1
+
+        penalties = 0.0
+
+        start_holds = 0
+        end_holds = 0
+        any_holds = 0
+        angle_tokens = 0
+        difficulty_tokens = 0
+
+        for pred in predictions:
+            unique_tokens = set()
+            for token in pred:
+                # No token should appear more than once
+                if token in unique_tokens:
+                    penalties += penalty_factor
+                unique_tokens.add(token)
+
+                if token in self.start_hold_tokens:
+                    start_holds += 1
+                elif token in self.end_hold_tokens:
+                    end_holds += 1
+                elif token in self.any_hold_tokens:
+                    any_holds += 1
+                elif token in self.angle_tokens:
+                    angle_tokens += 1
+                elif token in self.difficulty_tokens:
+                    difficulty_tokens += 1
+
+        if start_holds < 1:
+            penalties += penalty_factor
+        elif start_holds > 2:
+            penalties += (start_holds - 2) * penalty_factor
+
+        if end_holds < 1:
+            penalties += penalty_factor
+        elif end_holds > 2:
+            penalties += (end_holds - 2) * penalty_factor
+
+        if any_holds < 1:
+            penalties += penalty_factor
+
+        if difficulty_tokens < 1:
+            penalties += penalty_factor
+        elif difficulty_tokens > 1:
+            penalties += (difficulty_tokens - 1) * penalty_factor
+
+        if angle_tokens < 1:
+            penalties += penalty_factor
+        elif angle_tokens > 1:
+            penalties += (angle_tokens - 1) * penalty_factor
+
+        return penalties
